91e38be38e97ef8172c3e1ad11b868eef7c3a381
deepwaterooo <heyanh@vandals.uidaho.edu>
2015-05-17 22:22:25 -0700

    trial 2 ....

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include "mainwindow.h"
 #include "thread.h"
 
-
 int top[10]; // 1 2 4 5 6; 8 9 11 12 13
 int btm[15]; // 0 ~ 14
 //unsigned char notedata[6] = {SEQ_MIDIPUTC, 0, 0, 0, 0, 0};
@@ -17,5 +16,27 @@ int main(int argc, char *argv[]) {
     a.setQuitOnLastWindowClosed(true);
     MainWindow w;
     w.show();
+
+    // trial 2: put threads here
+    // yy
+    // player
+    /*
+    qDebug() << "main thread: " << QThread::currentThreadId();
+    QThread thread;
+
+    ReadFromMidiThread readthread;
+    WriteToMidiThread writethread;
+    
+    Player player;
+
+    QObject::connect(&readthread, SIGNAL(readSig()), &player, SLOT(slot_thread()));
+    player.moveToThread(&thread);
+    thread.start();
+
+    readthread.emitSig();
+
+    close(fd); // could this implements open() & close() once ?
+    */
+    
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -492,15 +492,14 @@ MainWindow::MainWindow(QWidget *parent)
     yy->writeMidi = 0;
     yy->isPlaying = 0;
     //connect(yy, SIGNAL(readUpdate()), this, SLOT(readFromDevice())); 
-    //connect(yy, SIGNAL(finished()), this, SLOT(display()));      //前面线程读完了不是发一个信号么，这个信号就是发到这个槽     
 
     QThread thread;
     Player player;
     QObject::connect(yy, SIGNAL(readUpdate()), &player, SLOT(readFromDevice()));
+    connect(yy, SIGNAL(finished()), this, SLOT(deleteLater()));      //前面线程读完了不是发一个信号么，这个信号就是发到这个槽
     
-    //player.moveToThread(&thread);
-    //thread.start();
-
+    player.moveToThread(&thread);
+    thread.start();
     
     // for rectangle "Bend" key connection
     QList<RenderArea*>::iterator it = renderAreas.begin();
